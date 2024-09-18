3b3aa762aeceec3cb61dd3c322d5624d883a4d7f
OceansForce <julengarcia165@gmail.com>
2024-09-18 08:57:55 +0000

    Probando Matrices de Arrays, foreach y JSON

--- Fitxategiak/Apunteak/Array_Matrizea_eta_Foreach.php
+++ Fitxategiak/Apunteak/Array_Matrizea_eta_Foreach.php
@@ -9,7 +9,11 @@
     <?php
     echo "<table border='1'>";
     $zenb1= array(array("Julen","Garcia","Mata"), array("Iker","Esnal","Etxebe"),array("Ana","Mata","Rodrigez"));
+    
+    //Escrito en formato JSON
     echo json_encode($zenb1, JSON_PRETTY_PRINT);
+
+    //Formato table html
     /*foreach ($zenb1 as $key) {
         
         foreach ($key as $key2) {
