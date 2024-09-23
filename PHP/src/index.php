6db8678da753af2f8b972254b4b3480dcdf5bca9
OceansForce <julengarcia165@gmail.com>
2024-09-23 11:56:50 +0000

    Cooki-ak egiten

--- PHP/src/index.php
+++ PHP/src/index.php
@@ -1,22 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-</head>
-<body>
-    <form method="POST" action="formulario.php">
-        Name:<input type="text" name="izena"><br>
-        E-mail:<input type="text" name="email"><br>
-        Password:<input type="password" name="pasaitza"><br>
-        Fitxategia:<input type="file"  name="file"><br>
-        Gender:
-            <input type="radio" name="gender" value="Gizona">Gizona 
-            <input type="radio" name="gender" value="emakumea">Emakumea
-            <input type="radio" name="gender" value="beste">Beste bat<br>
-        Textare:<textarea name="textare" rows="5" cols="40"></textarea><br>
-        <input type="submit">
-    </form>
-</body>
-</html>
+<?php 
+    $ondo;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (empty($_POST["izena"])){
+            echo "Ez da jarri IZENA";
+            $ondo=false;
+        }else{
+            echo $_POST["izena"];
+        }
+        echo "<br>";
+        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === true){
+            echo $_POST["email"];
+            
+        }else{
+            echo "Ez da email bat ".$_POST["email"];
+            $ondo=false;
+        }
+   
+        echo "<br>";
+        if (empty($_POST["pasaitza"])){
+            echo "Ez da jarri Pasahitza";
+            $ondo=false;
+        }else{
+            echo $_POST["pasaitza"];
+        }  
+        echo "<br>";
+    }
+
+    if ($ondo===true){
+        setcookie($_POST["izena"], $_POST["email"],$_POST["pasaitza"], time() + (86400 * 30), "/");
+    }
+?>
