804f86aa8aae1679dd2e26920ddb426376a60582
OceansForce <julengarcia165@gmail.com>
2024-11-13 09:57:39 +0000

    Denbora erakutsi eginda

--- PHP/src/denbora.php
+++ PHP/src/denbora.php
@@ -27,7 +27,7 @@
 
         $conn= $db -> konexioa();
 
-        $sql_ordu_aukera= "SELECT * FROM Iragarpena_orduko WHERE Eguna=$eguna and id_eguna= (SELECT Id_eguna FROM Iragarpena_eguna WHERE herria like '$herria' and Eguna=$eguna)";
+        $sql_ordu_aukera= "SELECT * FROM Iragarpena_orduko WHERE Eguna=$eguna and id_eguna= (SELECT Id_eguna FROM Iragarpena_eguna WHERE herria like '$herria' and eguna=$eguna)";
         $ordu_aukera= mysqli_query($conn, $sql_ordu_aukera);
         
         return $ordu_aukera;
@@ -42,6 +42,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    table, th, td {
+        border: 1px solid black;
+    }
+</style>
 <body>
     <form method="POST" action="denbora.php" >
         <input type="hidden" name="formulario_id" value="herria_form">
@@ -53,46 +58,44 @@
             ?>
         </select>
         <input type="submit">
-    </form>
+    </form> 
+
+    
+        
+            <?php
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    if($_POST["formulario_id"]=="herria_form"){
+                        echo "<table style='width:100%;text-align: center;'><tr style='width:100%'><th>Eguna</th><th style='width:20%'>Iragarpena-testua</th><th>Eguraldia</th><th>Minimoa</th><th>Maximoa</th><th>Orduak</th></tr>";
+                        $herria=$_POST["herriak"];
+                        $egunak=eguna_atera($herria);
+                        foreach ( $egunak as $value) {
+                            echo "<form method='POST' action='denbora.php' ><input type='hidden' name='formulario_id' value='eguna_form'><tr><td>".$value["eguna"]."</td><td >".$value["iragarpena-testua"]."</td><td>".$value["eguraldia"]."</td><td style='width:20%'>".$value["tenperatura-minimoa"]."</td><td>".$value["tenperatura-maximoa"]."</td><td><input type='hidden' name='ordua' value='".$value["eguna"].",".$herria."'><input type='submit' value='orduak'></td></form>";
+                        }
+                        echo  "</table>";
+                    }
+                }
+            ?>
+       
     
 
-    <form method="POST" action="denbora.php">
-        <input type="hidden" name="formulario_id" value="egunak_form">
-        <select name="egunak" id="egunak" name="form_">
-        <?php
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                if($_POST["formulario_id"]=="herria_form"){
-                    $herria=$_POST["herriak"];
-                    $egunak= eguna_atera($herria);
-                   
-                    foreach ($egunak as $key) {
-                        echo "<option value='".$key["eguna"]."'>".$key["eguna"]."</option>";                    
+    <form>
+            <?php
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    if($_POST["formulario_id"]=="eguna_form"){
+                        echo "<table style='width:100%;text-align: center;'><tr style='width:100%'><th>Ordua</th><th>Eguraldia</th><th>Temperatura</th><th>Prezipitazioa</th><th>Haizea-nondik</th><th>Haizea-km/h</th></tr>";
+                        $datuak=  explode(",", $_POST["ordua"]);
+                        $id= $datuak[0];
+                        $herria= $datuak[1];
+                        
+                        $orduak= ordua_atera($id, $herria);
+                    
+                        foreach ($orduak as $value) {
+                            echo "<tr><td>".$value["ordua"]."</td><td >".$value["eguraldia"]."</td><td>".$value["tenperatura"]."</td><td>".$value["prezipitazioa"]."</td><td>".$value["haizea-nondik"]."</td><td>".$value["haizea-km/h"]."</td>";
+                        }
+                        echo "</table>";
                     }
                 }
-            }
-        ?>
-        <input type="submit" >
-        </select>
-    </form>
-
-    <form method="POST" action="denbora.php">
-        <input type="hidden" name="formulario_id" value="ordua_form">
-        <select name="orduak" id="orduak" name="form_">
-        <?php
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                if($_POST["formulario_id"]=="egunak_form"){
-                    $eguna=$_POST["egunak"];
-                    echo $herria;
-                    $orduak= ordua_atera($eguna, $herria);
-                       
-                    foreach ($orduak as $key) {
-                        echo "<option value='".$key["orduak"]."'>".$key["ordua"]."</option>";                    
-                    }
-                }
-            }
-        ?>
-        <input type="submit" >
-        </select>
+            ?>
     </form>
     
 </body>
